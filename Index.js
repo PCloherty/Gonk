5ba3843fd5a428044986baf6aa9a459102f05f93
Paraic Cloherty <p.cloherty@hotmail.co.uk>
2020-04-24 13:42:21 +0100

    Gonk V1

---
+++ Index.js
@@ -0,0 +1,54 @@
+const { Client, MessageEmbed } = require("discord.js");
+const { config } = require("dotenv");
+const fetch = require("node-fetch");
+
+const client = new Client();
+const prefix = "!";
+
+client.once("ready", () => {
+  console.log("Ready!");
+});
+
+client.on("message", async (message) => {
+  if (!message.content.startsWith(prefix) || message.author.bot) return;
+
+  const args = message.content.slice(prefix.length).split(/ +/);
+  const command = args.shift().toLowerCase();
+
+  if (command === "set") {
+    console.log(`${args}`);
+
+    if (!args.length) {
+      return message.channel.send("Please supply a search term!");
+    }
+    const { sets } = await fetch(
+      `https://brickset.com/api/v3.asmx/getSets?apikey=${process.env.AP}&userHash=&params={'setNumber':'${args}-1','theme':'star wars'}`
+      
+    ).then((response) => response.json());
+    
+    console.log("fetch request done");
+    
+    if (!sets.length){
+        return message.channel.send(`Couldnt find anything with ${args}, please supply a valid Starwars set number`)
+    }
+
+    const [answer] = sets;
+    console.log("json turned into array");
+    const embed = new MessageEmbed()
+      .setTitle(answer.number)
+      .addFields(
+        { name: "Set Name", value: answer.name },
+        { name: "Released", value: answer.year}
+      )
+      .setImage(answer.image.imageURL);
+    console.log("message ready");
+    message.channel.send(embed);
+    console.log("message sent");
+  }
+});
+
+config({
+  path: __dirname + "/.env",
+});
+
+client.login(process.env.BT);
