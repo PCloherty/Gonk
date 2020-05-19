2cb52016db215417c01f066055f1f184933255ec
Paraic Cloherty <p.cloherty@hotmail.co.uk>
2020-05-19 16:54:00 +0100

    change variables from letter a to number to reflect what it contains

--- commands/functions/fetchQuery.js
+++ commands/functions/fetchQuery.js
@@ -4,10 +4,10 @@ module.exports = {
   name:"query",
   discription:"set number query function",
   execute(args){
-    datafetch = async(a) =>{   
-      console.log(a);
+    datafetch = async(num) =>{   
+      console.log(num);
       return await fetch(
-        `https://brickset.com/api/v3.asmx/getSets?apikey=${process.env.AP}&userHash=&params={'setNumber':'${a}-1','theme':'star wars'}`
+        `https://brickset.com/api/v3.asmx/getSets?apikey=${process.env.AP}&userHash=&params={'setNumber':'${num}-1','theme':'star wars'}`
         
       ).then((response) => response.json()).then(resjson => {return resjson});      
     };
